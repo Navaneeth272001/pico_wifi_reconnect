a95025b134619089e08e222b64ad5bb1993d9b4c
Navaneetha Krishnan V <113966455+Navaneeth272001@users.noreply.github.com>
2024-07-07 19:52:22 +0530

    Update df_test.py

--- df_test.py
+++ df_test.py
@@ -1,130 +1,89 @@
-  '''!
-    @brief Parse data returning from sensor
-    @param recv The obtained data
-  '''
-  def analysis_all_data(self,recv):
+# -*- coding: utf-8 -*
+'''
+  @file  read_gas_concentration.py
+  @brief Obtain gas concentration corresponding to the current environment, output as concentration value
+  @n Experimental mode: connect sensor communication pin to the main controller and burn
+  @n Experimental phenomenon: view the gas concentration corresponding to the current environment through serial port printing
+  @n Communication mode select, DIP switch SEL: 0: I2C, 1: UART
+  @n Group serial number         Address in the set
+  A0 A1 DIP level    00    01    10    11
+  @n 1            0x60  0x61  0x62  0x63
+  @n 2            0x64  0x65  0x66  0x67
+  @n 3            0x68  0x69  0x6A  0x6B
+  @n 4            0x6C  0x6D  0x6E  0x6F
+  @n 5            0x70  0x71  0x72  0x73
+  @n 6 (Default address group) 0x74  0x75  0x76  0x77 (Default address)
+  @n 7            0x78  0x79  0x7A  0x7B
+  @n 8            0x7C  0x7D  0x7E  0x7F
+  @n I2C address select, default to 0x77, A1 and A0 are grouped into 4 I2C addresses.
+  @n             | A0 | A1 |
+  @n             | 0  | 0  |    0x74
+  @n             | 0  | 1  |    0x75
+  @n             | 1  | 0  |    0x76
+  @n             | 1  | 1  |    0x77   default i2c address   
+  @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+  @license     The MIT License (MIT)
+  @author      PengKaixing(kaixing.peng@dfrobot.com)
+  @version     V2.0
+  @date        2021-03-28
+  @url         https://github.com/DFRobot/DFRobot_MultiGasSensor
+'''
+import sys
+import os
+import time
+import paho.mqtt.client as mqtt
 
-  '''
-  @brief Change the mode of reporting data to the main controller after the sensor has collected the gas.
-  @param INITIATIVE: The sensor proactively reports data
-         PASSIVITY : The sensor can report data only after the main controller sends request to it.
-  @return status
-          True is :  change success
-          False is:  change fail
-  '''
-  def change_acquire_mode(self,mode):
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+from DFRobot_MultiGasSensor import *
 
-  '''
-    @brief Get the gas concentration or type obtained by the sensor
-    @param gastype    : The current sensor type
-         O2 : oxygen
-         CO : carbonic oxide
-         H2S: hydrogen sulfide
-         NO2: nitrogen dioxide
-         O3 : ozone
-         CL2: chlorine
-         NH3: ammonia
-         H2 : hydrogen
-         HF : hydrogen fluoride
-         PH3: phosphine
-    @return If data is transmitted normally, return gas concentration
-      Otherwise, return 0xffff
-  '''
-  def read_gas_concentration(self):
+# MQTT Broker (Replace with your broker's IP address or hostname)
+MQTT_BROKER = "192.168.1.134"
+# MQTT Topic to publish gas concentration readings
+MQTT_TOPIC = "b"zurich/Gas"
 
-  '''
-    @brief Change the mode of reporting data to the main controller after the sensor has collected the gas.
-    @param INITIATIVE: The sensor proactively reports data
-         PASSIVITY : The sensor can report data only after the main controller sends request to it.
-    @return status
-          True is ： change success
-          False is： change fail
-  '''
-  def change_acquire_mode(self,mode):
+'''
+  ctype=1:UART
+  ctype=0:IIC
+'''
+ctype = 0
 
-  '''
-    @brief Get the gas concentration or type obtained by the sensor
-    @param gastype    : The current sensor type
-         O2 : oxygen
-         CO : carbonic oxide
-         H2S: hydrogen sulfide
-         NO2: nitrogen dioxide
-         O3 : ozone
-         CL2: chlorine
-         NH3: ammonia
-         H2 : hydrogen
-         HF : hydrogen fluoride
-         PH3: phosphine
-    @return If data is transmitted normally, return gas concentration
-      Otherwise, return 0xffff
-  '''
-  def read_gas_concentration(self):      
+if ctype == 0:
+    I2C_1 = 0x01  # I2C_1 Use i2c1 interface (or i2c0 with configuring Raspberry Pi) to drive sensor
+    I2C_ADDRESS = 0x77  # I2C Device address, which can be changed by changing A1 and A0,
+                        # the default address on the terminal board as shipped is 0x74
+    gas = DFRobot_MultiGasSensor_I2C(I2C_1, I2C_ADDRESS)
+else:
+    gas = DFRobot_MultiGasSensor_UART(9600)
 
-  '''
-    @brief Get the gas type obtained by the sensor
-    @param No
-    @return Gas type
-        O2   0x05
-        CO   0x04
-        H2S  0x03
-        NO2  0x2C
-        O3   0x2A
-        CL2  0x31
-        NH3  0x02
-        H2   0x06
-        HCL  0X2E
-        SO2  0X2B
-        HF   0x33
-        PH3  0x45
-  '''
-  def read_gas_type(self):  
+def setup():
+    # Mode of obtaining data: the main controller needs to request the sensor for data
+    while (False == gas.change_acquire_mode(gas.PASSIVITY)):
+        print("wait acquire mode change!")
+        time.sleep(1)
+    print("change acquire mode success!")
+    gas.set_temp_compensation(gas.ON)
+    time.sleep(1)
 
-  '''
-    @brief Set sensor alarm threshold
-    @param switchof    : Set whether to turn on alarm function
-           ON          : Turn on alarm function
-           OFF         : Turn off alarm function
-           threshold   : Set alarm threshold
-           returntype : 
-           GASCON     : Gas concentration
-    @return status  : init status
-            True is : init success
-            False is: init error
-  '''
-  def set_threshold_alarm(self,switchof,threshold,gasType):    
+def loop():
+    # Gastype is set while reading the gas level. Must first perform a read before
+    # attempting to use it.
+    con = gas.read_gas_concentration()
+    print("Ambient " + gas.gastype + " concentration: %.2f " % con + gas.gasunits + " temp: %.1fC" % gas.temp)
+    # Publish gas concentration reading to MQTT broker
+    publish_mqtt(con)
 
-  '''
-    @brief Get sensor onboard temperature
-    @param No
-    @return Return the current board temperature as float type
-  '''
-  def read_temp(self):    
+    time.sleep(1)
 
-  '''
-    @brief Set whether to turn on temperature compensation, values output by sensor under different temperatures are various.
-          To get more accurate gas concentration, temperature compensation are necessary when calculating gas concentration.
-    @param tempswitch：
-          ON          : Turn on temperature compensation
-          OFF         : Turn off temperature compensation
-    @return 无
-  '''
-  def set_temp_compensation(self,tempswitch):      
+def publish_mqtt(concentration):
+    # Connect to MQTT Broker
+    client = mqtt.Client()
+    client.connect(MQTT_BROKER, 1883, 60)
+    # Publish gas concentration to MQTT topic
+    client.publish(MQTT_TOPIC, "%.2f" % concentration)
+    # Disconnect from MQTT Broker
+    client.disconnect()
 
-  '''
-    @brief Set whether to turn on temperature compensation, values output by sensor under different temperatures are various.
-          To get more accurate gas concentration, temperature compensation are necessary when calculating gas concentration.
-    @param tempswitch：
-          ON          : Turn on temperature compensation
-          OFF         : Turn off temperature compensation
-    @return 无
-  '''
-  def set_temp_compensation(self,tempswitch):
-
-  '''
-    @brief Get sensor gas concentration output by original voltage, which is different from reading sensor register directly.
-          The function is mainly for detecting whether the read gas concentration is right.
-    @param  vopin：Pin for receiving the original voltage output from sensor probe
-    @return The original voltage output of sensor gas concentration
-  '''
-  def read_volatage_data(self): 
-        
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
